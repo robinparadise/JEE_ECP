5677247a351ff8604861bb797f1e9eff1cd79022
robinparadise <robinchogiles@gmail.com>
2015-03-13 18:01:03 +0100

    Implement class Tema

--- src/main/java/es/ecp/models/entities/Tema.java
+++ src/main/java/es/ecp/models/entities/Tema.java
@@ -11,8 +11,44 @@ public class Tema {
 	@GeneratedValue
 	private Integer id;
 
-    public Tema(){
-    	
+	private String name;
+	private String question;
+
+    public Tema(){	
     }
+    
+    public Tema(String name, String question){
+    	this.name = name;
+    	this.question = question;
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	@Override
+	public String toString() {
+		return "Tema [id=" + id + ", name=" + name + ":" + question + "]";
+	}
 
 }
