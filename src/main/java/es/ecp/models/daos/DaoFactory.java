c58c23bbc1db3609007f5497fba9a8fb68dd8bad
robinparadise <robinchogiles@gmail.com>
2015-03-14 17:20:13 +0100

    Copy classes (daos & daos jpa) from JEE.Persistencia

---
+++ src/main/java/es/ecp/models/daos/DaoFactory.java
@@ -0,0 +1,23 @@
+package es.ecp.models.daos;
+
+import es.ecp.models.daos.jpa.DaoJpaFactory;
+
+public abstract class DaoFactory {
+
+	public static DaoFactory factory = null;
+
+	public static void setFactory(DaoFactory factory) {
+		DaoFactory.factory = factory;
+	}
+
+	public static DaoFactory getFactory() {
+		//assert factory != null;
+		if (factory == null){
+        	factory = new DaoJpaFactory();
+        }
+		return factory;
+	}
+
+	public abstract TemaDao getTemaDao();
+
+}
