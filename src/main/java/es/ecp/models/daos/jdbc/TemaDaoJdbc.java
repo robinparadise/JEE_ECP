6e43773b18c5df17b7fbadc149937e4b194d525b
robinparadise <robinchogiles@gmail.com>
2015-03-15 20:05:02 +0100

    TemaDaoJDBC Class :: implement read method

--- src/main/java/es/ecp/models/daos/jdbc/TemaDaoJdbc.java
+++ src/main/java/es/ecp/models/daos/jdbc/TemaDaoJdbc.java
@@ -1,6 +1,11 @@
 package es.ecp.models.daos.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import es.ecp.models.daos.TemaDao;
 import es.ecp.models.entities.Tema;
@@ -11,10 +16,24 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements
 	private static final String SQL_CREATE_TABLE = "CREATE TABLE %s (%s INT NOT NULL AUTO_INCREMENT, %s VARCHAR(255) NOT NULL, "
 			+ "%s VARCHAR(255) NOT NULL, PRIMARY KEY (%s))";
 	private static final String SQL_INSERT = "INSERT INTO %s (%s,%s) VALUES ('%s','%s')";
+	
+	private Logger log = LogManager.getLogger(TemaDaoJdbc.class);
 
 	public static String sqlToCreateTable() {
 		return String.format(SQL_CREATE_TABLE, Tema.TABLE, Tema.ID, Tema.NAME,
 				Tema.QUESTION, Tema.ID);
+	}
+	
+	private Tema create(ResultSet resultSet) {
+		try {
+			if (resultSet != null && resultSet.next()) {
+				return new Tema(resultSet.getString(Tema.NAME),
+						resultSet.getString(Tema.QUESTION));
+			}
+		} catch (SQLException e) {
+			log.error("read: " + e.getMessage());
+		}
+		return null;
 	}
 	
 	@Override
@@ -22,6 +41,13 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements
 		this.updateSql(String.format(SQL_INSERT, Tema.TABLE, Tema.NAME,
 				Tema.QUESTION, tema.getName(), tema.getQuestion()));
 		tema.setId(this.autoId());
+	}
+	
+	@Override
+	public Tema read(Integer id) {
+		ResultSet resultSet = this.query(String.format(SQL_SELECT_ID,
+				Tema.TABLE, id));
+		return this.create(resultSet);
 	}
 
 	@Override
@@ -32,12 +58,6 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements
 
 	@Override
 	public List<Tema> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Tema read(Integer id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
