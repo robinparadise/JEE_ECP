61903e4935ab74da25115d03492cd46ee01a1543
robinparadise <robinchogiles@gmail.com>
2015-03-15 16:59:34 +0100

    Empty VotoTest Class

---
+++ src/main/java/es/ecp/models/daos/jpa/VotoDaoJpaTest.java
@@ -0,0 +1,14 @@
+package es.ecp.models.daos.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VotoDaoJpaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
