b9bcad8edd294657a98214abba7eacbe8ca9649a
robinparadise <robinchogiles@gmail.com>
2015-03-15 20:01:23 +0100

    TemaDaoJDBC Class :: implement create method

--- src/main/java/es/ecp/models/daos/jdbc/TemaDaoJdbc.java
+++ src/main/java/es/ecp/models/daos/jdbc/TemaDaoJdbc.java
@@ -10,10 +10,18 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements
 
 	private static final String SQL_CREATE_TABLE = "CREATE TABLE %s (%s INT NOT NULL AUTO_INCREMENT, %s VARCHAR(255) NOT NULL, "
 			+ "%s VARCHAR(255) NOT NULL, PRIMARY KEY (%s))";
+	private static final String SQL_INSERT = "INSERT INTO %s (%s,%s) VALUES ('%s','%s')";
 
 	public static String sqlToCreateTable() {
 		return String.format(SQL_CREATE_TABLE, Tema.TABLE, Tema.ID, Tema.NAME,
 				Tema.QUESTION, Tema.ID);
+	}
+	
+	@Override
+	public void create(Tema tema) {
+		this.updateSql(String.format(SQL_INSERT, Tema.TABLE, Tema.NAME,
+				Tema.QUESTION, tema.getName(), tema.getQuestion()));
+		tema.setId(this.autoId());
 	}
 
 	@Override
@@ -26,12 +34,6 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements
 	public List<Tema> findAll() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public void create(Tema entity) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
