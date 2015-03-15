60b2ee2675932312bd65d95ee6a04000eacc82a3
robinparadise <robinchogiles@gmail.com>
2015-03-15 16:51:56 +0100

    TemaTest Class :: implement deleteById

--- src/test/java/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/models/daos/jpa/TemaDaoJpaTest.java
@@ -13,46 +13,52 @@ import es.ecp.models.daos.jpa.DaoJpaFactory;
 import es.ecp.models.entities.Tema;
 
 public class TemaDaoJpaTest {
-	
+
 	private Tema tema;
 	private TemaDao temaDao = DaoJpaFactory.getFactory().getTemaDao();;
-	
+
 	@BeforeClass
-	public static void beforeClass(){
+	public static void beforeClass() {
 		DaoFactory.setFactory(new DaoJpaFactory());
 	}
 
 	@Before
-	public void before(){
+	public void before() {
 		this.tema = new Tema("name_test", "question_test");
 		temaDao.create(this.tema);
 	}
 
 	@Test
-	public void testCreate(){
+	public void testCreate() {
 		Tema tema = new Tema("name_t1", "q_t1");
 		temaDao.create(tema);
 		Tema tema1 = temaDao.read(tema.getId());
 		assertEquals(tema, tema1);
 		temaDao.deleteById(tema.getId());
 	}
-	
+
 	@Test
 	public void testRead() {
 		assertEquals(this.tema, temaDao.read(this.tema.getId()));
 	}
-	
+
 	@Test
-	public void testUpdate(){
+	public void testUpdate() {
 		this.tema.setName("name_updated");
 		this.tema.setQuestion("question_updated");
 		temaDao.update(this.tema);
 		assertEquals(this.tema, temaDao.read(this.tema.getId()));
 	}
-	
+
+	@Test
+	public void testDelete() {
+		temaDao.deleteById(this.tema.getId());
+		assertEquals(temaDao.read(this.tema.getId()), null);
+	}
+
 	@After
-    public void after() {
-        temaDao.deleteById(this.tema.getId());
-    }
+	public void after() {
+		temaDao.deleteById(this.tema.getId());
+	}
 
 }
