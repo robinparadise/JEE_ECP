af5fd156de7073163ca0023cbe67a3498347b982
robinparadise <robinchogiles@gmail.com>
2015-03-15 16:01:43 +0100

    Implement Voto::findByTema

--- src/main/java/es/ecp/models/daos/VotoDao.java
+++ src/main/java/es/ecp/models/daos/VotoDao.java
@@ -1,7 +1,11 @@
 package es.ecp.models.daos;
 
+import java.util.List;
+
 import es.ecp.models.entities.Voto;
 
 public interface VotoDao extends GenericDao<Voto, Integer> {
+
+	List<Voto> findByTemaId(Integer tema_id);
 
 }
--- src/main/java/es/ecp/models/daos/jpa/VotoDaoJpa.java
+++ src/main/java/es/ecp/models/daos/jpa/VotoDaoJpa.java
@@ -1,12 +1,33 @@
 package es.ecp.models.daos.jpa;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import es.ecp.models.daos.VotoDao;
 import es.ecp.models.entities.Voto;
 
 public class VotoDaoJpa extends GenericDaoJpa<Voto, Integer> implements VotoDao {
 
+	private EntityManager entityManager;
+
 	public VotoDaoJpa() {
 		super(Voto.class);
+	}
+
+	@Override
+	public List<Voto> findByTemaId(Integer tema_id) {
+		CriteriaBuilder criteria = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Voto> query = criteria.createQuery(Voto.class);
+		Root<Voto> rootVoto = query.from(Voto.class);
+		query.where(criteria.equal(
+				rootVoto.get("tema").get("id").as(Integer.class), tema_id));
+		TypedQuery<Voto> votacionQuery = entityManager.createQuery(query);
+		return votacionQuery.getResultList();
 	}
 
 }
