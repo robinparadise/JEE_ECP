a558dbe9184783565712ccd30e7a0864fbee9f36
robinparadise <robinchogiles@gmail.com>
2015-03-16 21:08:56 +0100

    Dispatcher :: GET index.jsp

--- src/main/java/es/ecp/views/beans/Dispatcher.java
+++ src/main/java/es/ecp/views/beans/Dispatcher.java
@@ -8,20 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+
 @WebServlet("/jsp/*")
 public class Dispatcher extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private static String PATH_ROOT_VIEW = "/views/jsp/";
 
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-	}
+		String view = "index";
+		LogManager.getLogger().debug(PATH_ROOT_VIEW + view + ".jsp");
+		this.getServletContext()
+				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+				.forward(request, response);
 
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
 	}
 
 }
