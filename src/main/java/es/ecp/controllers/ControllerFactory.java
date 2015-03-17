86388931ee5de9bf9a175c9b54bc212da2bd4a11
robinparadise <robinchogiles@gmail.com>
2015-03-17 20:49:50 +0100

    ControllerFactory :: getTemaController

--- src/main/java/es/ecp/controllers/ControllerFactory.java
+++ src/main/java/es/ecp/controllers/ControllerFactory.java
@@ -2,4 +2,12 @@ package es.ecp.controllers;
 
 public class ControllerFactory {
 
+	private TemaController temaController;
+
+	public TemaController getTemaController() {
+		if (temaController == null)
+			temaController = new TemaController();
+		return temaController;
+	}
+
 }
