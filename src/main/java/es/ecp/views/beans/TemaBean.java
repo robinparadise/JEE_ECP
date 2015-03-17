485661670983fc7fd3b1cc52e464997a6c82abb1
robinparadise <robinchogiles@gmail.com>
2015-03-17 19:43:06 +0100

    empty TemaBean Class

---
+++ src/main/java/es/ecp/views/beans/TemaBean.java
@@ -0,0 +1,12 @@
+package es.ecp.views.beans;
+
+import java.io.Serializable;
+
+public class TemaBean extends ViewBean implements Serializable {
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
