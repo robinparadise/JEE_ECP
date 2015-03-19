adf41b8a599a8bfd082dafb08d28b5ab5d73f503
robinparadise <robinchogiles@gmail.com>
2015-03-19 19:59:03 +0100

    ControllerFactory :: getAddTemaController method

--- src/main/java/es/ecp/controllers/ControllerFactory.java
+++ src/main/java/es/ecp/controllers/ControllerFactory.java
@@ -3,11 +3,18 @@ package es.ecp.controllers;
 public class ControllerFactory {
 
 	private TemaController temaController;
+	private AddTemaController addTemaController;
 
 	public TemaController getTemaController() {
 		if (temaController == null)
 			temaController = new TemaController();
 		return temaController;
+	}
+
+	public AddTemaController getAddTemaController() {
+		if (addTemaController == null)
+			addTemaController = new AddTemaController();
+		return addTemaController;
 	}
 
 }
