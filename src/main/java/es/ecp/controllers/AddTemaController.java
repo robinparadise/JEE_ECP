0a08c77514c205da8e9b7bc19ed4df6c10f2985a
robinparadise <robinchogiles@gmail.com>
2015-03-19 19:46:33 +0100

    Add Empty Class TemaController

---
+++ src/main/java/es/ecp/controllers/AddTemaController.java
@@ -0,0 +1,12 @@
+package es.ecp.controllers;
+
+import es.ecp.models.daos.TemaDao;
+import es.ecp.models.daos.jpa.DaoJpaFactory;
+import es.ecp.models.entities.Tema;
+
+public class AddTemaController {
+	
+	public void add(Tema tema) {
+	}
+
+}
