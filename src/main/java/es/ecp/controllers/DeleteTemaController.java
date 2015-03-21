af7faaacd33d1672bf73c4bc76e645fc6adb89d9
robinparadise <robinchogiles@gmail.com>
2015-03-21 21:14:17 +0100

    DeleteTemaController :: deleteById method

--- src/main/java/es/ecp/controllers/DeleteTemaController.java
+++ src/main/java/es/ecp/controllers/DeleteTemaController.java
@@ -1,8 +1,23 @@
 package es.ecp.controllers;
 
+import java.util.List;
+
+import es.ecp.models.daos.TemaDao;
+import es.ecp.models.daos.VotoDao;
+import es.ecp.models.daos.jpa.DaoJpaFactory;
+import es.ecp.models.entities.Voto;
+
 public class DeleteTemaController {
 
-	public void deleteById(Integer temaid) {
+	VotoDao votoDao = DaoJpaFactory.getFactory().getVotoDao();
+	TemaDao temaDao = DaoJpaFactory.getFactory().getTemaDao();
 
+	public void deleteById(Integer temaid) {
+		List<Voto> votos = votoDao.findByTemaId(temaid);
+		
+		for (Voto voto : votos) {
+			votoDao.deleteById(voto.getId());
+		}
+		temaDao.deleteById(temaid);
 	}
 }
