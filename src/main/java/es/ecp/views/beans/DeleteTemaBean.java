7844579c2dae532cc63bf6cbfe3bde76d90df4cc
robinparadise <robinchogiles@gmail.com>
2015-03-21 20:57:33 +0100

    DeleteTemabean :: update method

--- src/main/java/es/ecp/views/beans/DeleteTemaBean.java
+++ src/main/java/es/ecp/views/beans/DeleteTemaBean.java
@@ -1,5 +1,20 @@
 package es.ecp.views.beans;
 
-public class DeleteTemaBean {
+import java.io.Serializable;
+
+import es.ecp.models.daos.jpa.DaoJpaFactory;
+import es.ecp.models.entities.Tema;
+
+public class DeleteTemaBean extends ViewBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Tema tema;
+
+	public void setTemaId(Integer id) {
+		this.tema = DaoJpaFactory.getFactory().getTemaDao().read(id);
+	}
+	
+	public void update() {
+		this.getControllerFactory().getDeleteTemaController().deleteById(tema.getId());
+	}
 
 }
