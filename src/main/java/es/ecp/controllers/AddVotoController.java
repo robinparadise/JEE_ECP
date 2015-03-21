4d02bf0bcee8af29386961c567bc231e414376c3
robinparadise <robinchogiles@gmail.com>
2015-03-21 17:41:13 +0100

    AddVotoController :: add method

--- src/main/java/es/ecp/controllers/AddVotoController.java
+++ src/main/java/es/ecp/controllers/AddVotoController.java
@@ -1,6 +1,14 @@
 package es.ecp.controllers;
 
+import es.ecp.models.daos.VotoDao;
+import es.ecp.models.daos.jpa.DaoJpaFactory;
+import es.ecp.models.entities.Voto;
 
 public class AddVotoController {
+	
+	public void add(Voto voto) {
+		VotoDao votoDao = DaoJpaFactory.getFactory().getVotoDao();
+		votoDao.create(voto);
+	}
 
 }
