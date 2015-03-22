33fe3402e9da10430c9df54108bbf3d333ade4f4
robinparadise <robinchogiles@gmail.com>
2015-03-22 19:43:18 +0100

    ShowVotosBean :: calc Media by Nivel Estudios

--- src/main/java/es/ecp/views/beans/ShowVotosBean.java
+++ src/main/java/es/ecp/views/beans/ShowVotosBean.java
@@ -19,6 +19,7 @@ public class ShowVotosBean extends ViewBean implements Serializable {
 	private VotoDao votoDao = DaoJpaFactory.getFactory().getVotoDao();
 	private int media = 0;
 	private Map<NivelEstudios, Integer> mediaByNivelEstudios;
+	private int numVotos;
 
 	public void setTemaId(Integer temaid) {
 		this.tema = DaoJpaFactory.getFactory().getTemaDao().read(temaid);
@@ -49,7 +50,7 @@ public class ShowVotosBean extends ViewBean implements Serializable {
 				}
 				media = media + voto.getValoracion();
 			}
-			mediaByLevels.put(key, media / size);
+			if (size > 0) mediaByLevels.put(key, media / size);
 		}
 
 		return mediaByLevels;
@@ -72,9 +73,22 @@ public class ShowVotosBean extends ViewBean implements Serializable {
 	public Map<NivelEstudios, Integer> getMediaByNivelEstudios() {
 		return this.mediaByNivelEstudios;
 	}
+	public int getMediaByNivelEstudios(NivelEstudios nivel) {
+		return this.mediaByNivelEstudios.get(nivel);
+	}
+	public int getMediaByNivelEstudios(Integer nivel) {
+		return this.mediaByNivelEstudios.get((int) nivel);
+	}
+	public Tema getTema() {
+		return this.tema;
+	}
+	public int getNumVotos() {
+		return this.numVotos;
+	}
 	
 	public void update() {
 		votos = votoDao.findByTemaId(tema.getId());
+		this.numVotos = votos.size();
 		this.media = calcMedia(votos);
 		Map<NivelEstudios, List<Voto>> votosByHash = VotosByHash(votos);
 		this.mediaByNivelEstudios = calcMediaByNivelEstudios(votosByHash);
