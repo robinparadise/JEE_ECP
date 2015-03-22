9068560e7cef400e0a7e54311b780008f11c243e
robinparadise <robinchogiles@gmail.com>
2015-03-22 18:59:12 +0100

    Fix Max and Min values for valoracion

--- src/main/java/es/ecp/models/entities/Voto.java
+++ src/main/java/es/ecp/models/entities/Voto.java
@@ -1,15 +1,15 @@
 package es.ecp.models.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 import es.ecp.models.utils.NivelEstudios;
 
@@ -35,6 +35,8 @@ public class Voto {
 	private NivelEstudios nivel_estudios;
 
 	@Column(name = VALORACION, nullable = false)
+	@Max(value = 10)
+    @Min(value = 0)
 	private Integer valoracion;
 
 	@ManyToOne
