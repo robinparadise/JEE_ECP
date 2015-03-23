dfc6ab850f47e936d3f8d9385e58053cc9069c25
robinparadise <robinchogiles@gmail.com>
2015-03-23 19:21:13 +0100

    Rename AddTemaBean

--- src/main/java/es/ecp/views/beans/AddTemaBean.java
+++ src/main/java/es/ecp/views/beans/AddTemaBean.java
@@ -2,15 +2,27 @@ package es.ecp.views.beans;
 
 import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+
 import es.ecp.controllers.AddTemaController;
 import es.ecp.models.entities.Tema;
 
-public class AddTemaView extends ViewBean implements Serializable {
+@ManagedBean
+public class AddTemaBean extends ViewBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Tema tema;
+	private String name = "";
+	private String question = "";
+	
+	public AddTemaBean() {
+		super();
+		this.tema = new Tema();
+	}
 
-	public AddTemaView(String name, String question) {
+	public AddTemaBean(String name, String question) {
 		this.tema = new Tema(name, question);
+		this.name = tema.getName();
+		this.question = tema.getQuestion();
 	}
 
 	public Tema getTema() {
@@ -19,19 +31,30 @@ public class AddTemaView extends ViewBean implements Serializable {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+		this.name = tema.getName();
+		this.question = tema.getQuestion();
+	}
+	public String getName() {
+		return name;
+	}
+	public String getQuestion() {
+		return question;
 	}
 	
 	public void setName(String name) {
 		this.tema.setName(name);
+		this.name = name;
 	}
 	
 	public void setQuestion(String ques) {
 		this.tema.setQuestion(ques);
+		this.question = ques;
 	}
 
-	public void update() {
+	public String process() {
 		AddTemaController addTemaController = this.getControllerFactory().getAddTemaController();
 		addTemaController.add(tema);
+		return "newtema";
 	}
 
 }
